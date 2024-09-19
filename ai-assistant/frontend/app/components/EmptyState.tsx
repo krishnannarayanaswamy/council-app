33eb061a02038d8fe6938275e874e02058685115
Krishnan Narayana swamy <krishnan.narayanaswamy@datastax.com>
2024-09-19 23:26:18 +1000

    first commit

---
+++ ai-assistant/frontend/app/components/EmptyState.tsx
@@ -0,0 +1,62 @@
+import { MouseEvent } from "react";
+import {
+  Heading,
+  Link,
+  Card,
+  CardHeader,
+  Flex,
+  Spacer,
+} from "@chakra-ui/react";
+
+export function EmptyState(props: { onChoice: (question: string) => any }) {
+  const handleClick = (e: MouseEvent) => {
+    props.onChoice((e.target as HTMLDivElement).innerText);
+  };
+  return (
+    <div className="rounded flex flex-col items-center max-w-full md:p-8">
+      <Flex marginTop={"25px"} grow={1} maxWidth={"800px"} width={"100%"}>
+        <Card
+          onMouseUp={handleClick}
+          width={"48%"}
+          backgroundColor={"rgb(58, 58, 61)"}
+          _hover={{ backgroundColor: "rgb(78,78,81)" }}
+          cursor={"pointer"}
+          justifyContent={"center"}
+        >
+          <CardHeader justifyContent={"center"}>
+            <Heading
+              fontSize="lg"
+              fontWeight={"medium"}
+              mb={1}
+              color={"gray.200"}
+              textAlign={"center"}
+            >
+              What events are happening?
+            </Heading>
+          </CardHeader>
+        </Card>
+        <Spacer />
+        <Card
+          onMouseUp={handleClick}
+          width={"48%"}
+          backgroundColor={"rgb(58, 58, 61)"}
+          _hover={{ backgroundColor: "rgb(78,78,81)" }}
+          cursor={"pointer"}
+          justifyContent={"center"}
+        >
+          <CardHeader justifyContent={"center"}>
+            <Heading
+              fontSize="lg"
+              fontWeight={"medium"}
+              mb={1}
+              color={"gray.200"}
+              textAlign={"center"}
+            >
+              How do i make council payments?
+            </Heading>
+          </CardHeader>
+        </Card>
+      </Flex>
+    </div>
+  );
+}
